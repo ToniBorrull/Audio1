f69c31cca286fabda087a545a54017afbe2679b6
Toni <toni.borrull@enti.cat>
2024-05-14 23:50:29 +0200

    Birds, steps (finally)

--- Assets/stepsSound.cs
+++ Assets/stepsSound.cs
@@ -6,10 +6,17 @@ public class stepsSound : MonoBehaviour
 {
     public AudioSource aS;
     public AudioClip[] steps;
+    public AudioClip[] waterSteps;
+
+    public int type;
+
+    public CapsuleCollider playerColl;
     // Start is called before the first frame update
     void Start()
     {
         aS = GetComponent<AudioSource>();
+        playerColl = GetComponent<CapsuleCollider>();
+        type = 1;
     }
 
     // Update is called once per frame
@@ -18,9 +25,39 @@ public class stepsSound : MonoBehaviour
         
     }
 
-    void Step() {
+    void Step()
+    {
+        if (type == 1) { 
+            GrassStep();
+        }
+        if (type == 2)
+        {
+            WaterStep();
+        }
+    }
 
+    void GrassStep() 
+    {
         aS.clip = steps[Random.Range(0, steps.Length)];
-       aS.Play();
+        aS.Play();
+    
+    }
+    void WaterStep()
+    {
+            aS.clip = waterSteps[Random.Range(0, waterSteps.Length)];
+            aS.Play();
+        
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("water"))
+        {
+            type = 2;
+        }
+        if (other.gameObject.CompareTag("grass"))
+        {
+            type = 1;
+        }
+
     }
 }
